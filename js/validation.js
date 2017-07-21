6524972ac7328b04deb3faf62fa0ef5bcba136a0
Emersson Chew <emerssonchewcs@gmail.com>
2017-07-21 18:07:10 +1200

    Renamed image folder and deleted validation example
    Renamed image folder and deleted validation example.

--- js/validation.js
+++
@@ -1,6 +0,0 @@
-function validate() {
-  var email = document.getElementById('email').value;
-  var msg = document.getElementById('msg');
-  msg.className = "alert alert-success";
-  msg.innerHTML = email + ' has successfully registered / logged in.';
-}
